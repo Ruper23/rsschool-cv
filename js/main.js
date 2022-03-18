f7e5dba20cd80ed6de0b268d345142c3f82a79bd
Ruper23 <64148185+Ruper23@users.noreply.github.com>
2022-03-18 14:12:18 +0300

    feat: update js file

--- js/main.js
+++ js/main.js
@@ -4,23 +4,35 @@ const wayupJs = document.querySelector('#wayup-js');
 const wuJsPopUp = document.querySelector('.course-wu-js');
 const wayupFigma = document.querySelector('#wayup-figma');
 const wuFigmaPopUp = document.querySelector('.course-wu-figma');
+const rsschool = document.querySelector('#rsschool');
+const rsschoolPopUp = document.querySelector('.course-rsschool')
 
 geek.addEventListener('click', () => {
 	geekPopUp.classList.toggle('active');
 	wuJsPopUp.classList.remove('active');
 	wuFigmaPopUp.classList.remove('active');
+	rsschoolPopUp.classList.remove('active');
 
 })
 wayupJs.addEventListener('click', () => {
 	wuJsPopUp.classList.toggle('active');
 	geekPopUp.classList.remove('active');
 	wuFigmaPopUp.classList.remove('active');
+	rsschoolPopUp.classList.remove('active');
 
 })
 wayupFigma.addEventListener('click', () => {
 	wuFigmaPopUp.classList.toggle('active');
 	wuJsPopUp.classList.remove('active');
 	geekPopUp.classList.remove('active');
+	rsschoolPopUp.classList.remove('active');
+
+})
+rsschool.addEventListener('click', () => {
+	rsschoolPopUp.classList.toggle('active');
+	wuJsPopUp.classList.remove('active');
+	geekPopUp.classList.remove('active');
+	wuFigmaPopUp.classList.remove('active');
 
 })
 // burger menu
